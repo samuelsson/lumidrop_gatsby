0bdacf0ff8341c1da4e24053bc5ff5dc35cbb4fd
Erik Samuelsson <1119465+samuelsson@users.noreply.github.com>
2019-05-09 20:11:58 +0200

    Add some styling and nvmrc

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import 'normalize.css';
+import styles from './Layout.module.scss';
 
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
@@ -7,7 +8,9 @@ import Footer from "../Footer/Footer";
 const Layout = ({ children }) => (
   <div className="wrapper">
     <Header/>
-    {children}
+    <div className={ styles.content }>
+      {children}
+    </div>
     <Footer/>
   </div>
 );
