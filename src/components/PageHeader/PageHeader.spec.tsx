711d7812d8f08caabf08860be3f15f2fa581d15e
Erik Samuelsson <1119465+samuelsson@users.noreply.github.com>
2019-07-02 22:29:18 +0200

    test: 💍 add test test (yes, test test) for PageHeader

--- src/components/PageHeader/PageHeader.spec.tsx
+++ src/components/PageHeader/PageHeader.spec.tsx
@@ -3,11 +3,16 @@ import { render, cleanup } from '@testing-library/react';
 import PageHeader from './PageHeader';
 
 describe('<PageHeader />', (): void => {
-  beforeEach(cleanup);
+  afterEach(cleanup);
 
   it('renders the title', (): void => {
     const title = 'Header';
     const { getByText } = render(<PageHeader />);
     expect(getByText(title).tagName).toBeTruthy();
+  });
+
+  it('matches snapshot', (): void => {
+    const { asFragment } = render(<PageHeader />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
